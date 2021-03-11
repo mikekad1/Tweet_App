4886014d9fed7d34259864417293bb3b7819577f
ogpagan <mishakadenko@mail.ru>
2021-03-11 16:28:57 -0500

    added User model
    Added User model. Sign-up, user details/edit page and flash-messages.

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_08_221452) do
+ActiveRecord::Schema.define(version: 2021_03_11_201210) do
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
